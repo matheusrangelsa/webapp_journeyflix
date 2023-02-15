8883f115c40479b76fc131b30f723ef12807241b
matheusrangelsa <matheusrangelsa@gmail.com>
2023-02-15 01:42:13 -0300

    limpando arquivos html

--- meu_site.py
+++ meu_site.py
@@ -9,14 +9,6 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/contatos")
-def contatos():
-    return render_template("contatos.html")
-
-@app.route("/usuarios/<nome_usuario>")
-def usuarios(nome_usuario):
-    return render_template("usuarios.html", nome_usuario=nome_usuario)
-
 @app.route("/globoplay/entrelinhas")
 def entrelinhas():
     return render_template('entrelinhas.html')
