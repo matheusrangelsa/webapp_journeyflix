5af0214ddd26309a0204909f98421508d61899cf
matheusrangelsa <matheusrangelsa@gmail.com>
2023-02-16 15:55:31 -0300

    ...

--- meu_site.py
+++ meu_site.py
@@ -13,6 +13,13 @@ def homepage():
 def entrelinhas():
     return render_template('entrelinhas.html')
 
+@app.route('/resultado', methods=['POST'])
+def resultado():
+    trajeto = int(request.form['trajeto'])
+    # Aqui você pode inserir seu código de filtro para gerar a lista de resultados
+    resultados = ['Resultado 1', 'Resultado 2', 'Resultado 3']
+    return render_template('resultado.html', resultados=resultados)
+
 # colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
